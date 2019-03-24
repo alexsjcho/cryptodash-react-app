03096d8c3adde465526b5469a11bf773fa796dce
Alex Cho <alexsjcho@gmail.com>
2019-03-23 17:20:16 -0700

    Added styled component dependency

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,18 @@
+//React Imports
 import React, { Component } from "react";
+import styled, { css } from "styled-components";
+
+//Other Local File Imports
 import "./App.css";
+
+//Components Imports
+import Welcome from "./WelcomeMessage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Index Component</h1>
+        <Welcome />
       </div>
     );
   }
